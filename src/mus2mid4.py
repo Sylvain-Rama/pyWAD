5dd07069c48699662a8d8960611ffc591eb781d3
Sylvain <rama.sylvain@gmail.com>
2025-03-22 18:15:54 +0000

    Manage MUS & MID!

--- src/mus2mid4.py
+++ src/mus2mid4.py
@@ -3,14 +3,18 @@ from dataclasses import dataclass
 from enum import IntEnum
 from typing import BinaryIO
 from loguru import logger
+import shutil
 
 """Translation to Python and slight adaptation of mus2mid.c by Ben Ryves, 2006. 
-See https://svn.prboom.org/repos/tags/prboom-plus-2.5.0.1/src/mus2mid.c"""
+See https://svn.prboom.org/repos/tags/prboom-plus-2.5.0.1/src/mus2mid.c
+Added MIDI management by simply renaming the MIDI lump to .mid."""
 
 # Constants
 NUM_CHANNELS = 16
 MIDI_PERCUSSION_CHAN = 9
 MUS_PERCUSSION_CHAN = 15
+MUS_ID = b"MUS\x1a"
+MIDI_ID = b"MThd"
 
 
 # MUS event codes
@@ -45,7 +49,7 @@ class MusHeader:
 
 
 # MIDI header
-midiheader = b"MThd" + struct.pack(">IHHH", 6, 0, 1, 0x46) + b"MTrk" + struct.pack(">I", 0)
+midiheader = MIDI_ID + struct.pack(">IHHH", 6, 0, 1, 0x46) + b"MTrk" + struct.pack(">I", 0)
 
 # Global state
 channelvelocities = [127] * NUM_CHANNELS
@@ -123,9 +127,6 @@ def mus2mid(musinput: BinaryIO, midioutput: BinaryIO) -> None:
     global queuedtime, tracksize
     musfileheader = read_mus_header(musinput)
 
-    if musfileheader.id != b"MUS\x1a":
-        raise ValueError("Not a MUS file")
-
     musinput.seek(musfileheader.scorestart)
     midioutput.write(midiheader)
     tracksize = 0
@@ -192,3 +193,21 @@ def mus2mid(musinput: BinaryIO, midioutput: BinaryIO) -> None:
     # Write track size
     midioutput.seek(18)
     midioutput.write(struct.pack(">I", tracksize))
+
+
+def get_music(file_input: str, file_output: str):
+    with open(file_input, "rb") as musinput, open(file_output, "wb") as midioutput:
+        header_id = struct.unpack("<4s", musinput.read(4))[0]
+
+        if header_id == MUS_ID:
+            logger.info("MUS file")
+            musinput.seek(0)
+            mus2mid(musinput, midioutput)
+            return
+
+    if header_id == MIDI_ID:
+        logger.info("MIDI file")
+        shutil.copy(file_input, file_output)
+
+    else:
+        logger.info(f"Music format not recognised: {header_id}.")
