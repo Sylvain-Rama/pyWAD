d0c84f58d0bcaeb09486929cfeb2b85c02ee7aa9
Sylvain <rama.sylvain@gmail.com>
2025-04-20 20:20:34 +0100

    Manages both midi and ogg

--- st_pages/musics.py
+++ st_pages/musics.py
@@ -2,11 +2,14 @@ import streamlit as st
 from src.WADPlayer import MIDIPlayer
 from src.mus2mid import Mus2Mid
 import os
+from loguru import logger
+
 
 if "music_path" not in st.session_state:
     st.session_state["player"] = None
     st.session_state["music_path"] = None
     st.session_state["chosen_music"] = None
+    st.session_state["current_music"] = None
 
 col1, col2, col3, col4, _ = st.columns([2, 1, 1, 3, 3], vertical_alignment="bottom")
 
@@ -14,18 +17,22 @@ music_names = st.session_state["wad"].musics
 
 with col1:
     chosen_music = st.selectbox("Music", music_names)
-    if chosen_music
 
-    music_path = st.session_state["wad"].export_music(chosen_music)
+    if chosen_music != st.session_state["chosen_music"]:
 
-    if music_path.endswith(".mus"):
-        m2m = Mus2Mid(music_path)
+        music_path = st.session_state["wad"].export_music(chosen_music)
 
-        new_path = m2m.to_midi()
-        os.remove(music_path)
-        st.session_state["music_path"] = new_path
-    else:
-        st.session_state["music_path"] = music_path
+        if music_path.endswith(".mus"):
+            m2m = Mus2Mid(music_path)
+
+            new_path = m2m.to_midi()
+            os.remove(music_path)
+            st.session_state["music_path"] = new_path
+        else:
+            st.session_state["music_path"] = music_path
+
+        logger.debug(music_path)
+        st.session_state["chosen_music"] = chosen_music
 
 
 # Create the player if it's not created yet or if the music is changed
@@ -34,7 +41,7 @@ if st.session_state["music_path"].endswith(".mid"):
         try:
             st.session_state["player"] = MIDIPlayer(st.session_state["music_path"])
         except Exception as e:
-            st.error(f"Error loading music: {e}")
+            st.error(f"Error loading music: {e}.")
         st.session_state["current_music"] = chosen_music
 
     if st.session_state["current_music"] != chosen_music:
