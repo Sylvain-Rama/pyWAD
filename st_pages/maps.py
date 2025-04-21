c8740ca74ca0b0ec2fa0c44fcc1eb62fefd8358c
Sylvain <rama.sylvain@gmail.com>
2025-04-21 21:26:50 +0100

    typos

--- st_pages/maps.py
+++ st_pages/maps.py
@@ -13,9 +13,9 @@ with col3:
     show_things = things == "Dots"
 with col4:
     show_secrets = st.checkbox("Show Secrets")
-    show_special = st.checkbox("Show Special", value=True)
+    show_specials = st.checkbox("Show Special", value=True)
 
 fig = st.session_state["viewer"].draw_map(
-    chosen_map, palette=palette, show_secrets=show_secrets, show_specials=show_special, show_things=show_things
+    chosen_map, palette=palette, show_secrets=show_secrets, show_specials=show_specials, show_things=show_things
 )
 st.pyplot(fig, use_container_width=True, format="png", dpi=300)
