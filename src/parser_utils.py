d5db3001f5da69ada883b1fc2a4d5d30494785bd
Sylvain <rama.sylvain@gmail.com>
2025-04-25 17:49:28 +0100

    update

--- src/parser_utils.py
+++ src/parser_utils.py
@@ -2,7 +2,7 @@ import re
 
 
 EXMY_REGEX = re.compile(r"^E(?P<episode>[0-9])M(?P<number>[0-9])$")
-MAPXY_REGEX = re.compile(r"^MAP(?P<number>[0-9]{2})$")
+MAPXY_REGEX = re.compile(r"^MAP(?P<number>[0-9]{2}[A-Z]?)$")
 TEX_REGEX = re.compile(r"^TEXTURE.$")
 
 MAPS_ATTRS = {
