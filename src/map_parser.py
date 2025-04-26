b382858c8fb85537f36952722775256b33e9aaeb
Sylvain <rama.sylvain@gmail.com>
2025-04-26 22:57:31 +0100

    update map parser

--- src/map_parser.py
+++ src/map_parser.py
@@ -3,37 +3,55 @@ import re
 from collections import defaultdict
 import numpy as np
 from loguru import logger
+from typing import Union
 from dataclasses import dataclass
 
 
-def filter_flags_by_bit(flags: np.array, bit_position: int) -> np.array:
+@dataclass
+class ParsedMap:
+    map_lims: tuple[float, float, float, float]
+    map_dims: tuple[float, float]
+    map_name: str
+    block: np.array
+    twosided: np.array
+    special: np.array
+    things: dict[int, list[float, float]]
+    grouped_things: np.array
+
+
+def filter_flags_by_bit(flags: np.array, bit_position: int, bit_value=1) -> np.array:
     """
-    Returns the indices of flags where the given bit_position is set to 1.
+    Returns the indices of flags where the given bit_position is set to value.
     """
-    mask = (flags & (1 << bit_position)) != 0
+    mask = (flags & (1 << bit_position)) == bit_value
     return np.where(mask)[0]
 
 
-def parse_old_format(wad, map_name: str) -> dict:
-
-    map_info = defaultdict(list)
-    metadata = {}
-
-    lump = wad._lump_data(*wad._maps_lumps[map_name]["VERTEXES"])
-    vertices = np.array([struct.unpack("<hh", lump[i : i + 4]) for i in range(0, len(lump), 4)])
+def get_map_dims(vertices, parsed_map: ParsedMap) -> dict[str, float]:
 
     map_lims = (vertices[:, 0].min(), vertices[:, 0].max(), vertices[:, 1].min(), vertices[:, 1].max())
-    metadata["map_lims"] = map_lims
-    metadata["map_size"] = (map_lims[1] - map_lims[0], map_lims[3] - map_lims[2])
-    metadata["map_name"] = map_name
+    parsed_map.map_lims = map_lims
+    parsed_map.map_size = (map_lims[1] - map_lims[0], map_lims[3] - map_lims[2])
 
-    lump = wad._lump_data(*wad._maps_lumps[map_name]["LINEDEFS"])
+    return parsed_map
+
+
+def parse_old_format(wad, parsed_map: ParsedMap) -> ParsedMap:
+
+    map_dict = wad._maps_lumps[parsed_map.map_name]
+
+    lump = wad._lump_data(*map_dict["VERTEXES"])
+    vertices = np.array([struct.unpack("<hh", lump[i : i + 4]) for i in range(0, len(lump), 4)])
+
+    parsed_map = get_map_dims(vertices, parsed_map)
+
+    lump = wad._lump_data(*map_dict["LINEDEFS"])
 
     if wad.game_type in ["DOOM", "HERETIC"]:
 
         linedefs = np.array([struct.unpack("<HHHHHHH", lump[i : i + 14]) for i in range(0, len(lump), 14)])
 
-    elif wad.game_type in ["HEXEN"]:
+    elif wad.game_type in ["HEXEN", "UDMF"]:
         linedefs = np.array([struct.unpack("<HHHBBBBBBHH", lump[i : i + 16]) for i in range(0, len(lump), 16)])
 
     else:
@@ -45,18 +63,14 @@ def parse_old_format(wad, map_name: str) -> dict:
     flags = linedefs[:, 2]
     specials = linedefs[:, 3]
 
-    map_info["block"] = lines[filter_flags_by_bit(flags, 0)]  # Impassable bit is 0th bit (1 << 0)
-
     # Some WADs don't have all their linedefs flags properly set.
-    # We consider the value of 0 as blocking (it should be 1).
-    no_flags = lines[np.where(flags == 0)[0]]
-    map_info["block"] = np.concatenate((map_info["block"], no_flags), axis=0)
+    # We consider every lines that are not two-sided are blocking
+    parsed_map.block = lines[filter_flags_by_bit(flags, 2, bit_value=0)]
 
-    map_info["two-sided"] = lines[filter_flags_by_bit(flags, 2)]  # Two-sided
-    map_info["secret"] = lines[filter_flags_by_bit(flags, 5)]  # Secrets
-    map_info["special"] = lines[np.where(specials != 0)[0]]  # specials
+    parsed_map.twosided = lines[filter_flags_by_bit(flags, 2)]  # Two-sided
+    parsed_map.special = lines[np.where(specials != 0)[0]]  # specials
 
-    lump = wad._lump_data(*wad._maps_lumps[map_name]["THINGS"])
+    lump = wad._lump_data(*map_dict["THINGS"])
     things = np.array([struct.unpack("<HHHHH", lump[i : i + 10]) for i in range(0, len(lump), 10)]).astype(np.int16)
 
     things_dict = {}
@@ -73,7 +87,105 @@ def parse_old_format(wad, map_name: str) -> dict:
     # Simple way to get everything for plotting in the maps, but will keep the NONE keys.
     things_dict["all_things"] = {"x": things[:, 0], "y": things[:, 1]}
 
-    map_info["things"] = things_dict
-    map_info["metadata"] = metadata
+    parsed_map.things = things_dict
 
-    return map_info
+    return parsed_map
+
+
+def parse_value(value: str) -> Union[str, float, bool]:
+
+    value = value.strip()
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+    if value.startswith('"') and value.endswith('"'):
+        return value[1:-1]
+    try:
+        if "." in value:
+            return float(value)
+        else:
+            return int(value)
+    except ValueError:
+        return value  # fallback
+
+
+def parse_udmf_format(wad, parsed_map: ParsedMap) -> ParsedMap:
+    # See https://github.com/ZDoom/gzdoom/blob/master/specs/udmf.txt
+    # Thanks ChatGPT for the regexes.
+
+    vertices = []
+    blocking = []
+    twosided = []
+    special = []
+    things = []
+
+    text = re.sub(r"//.*", "", text)
+    text = re.sub(r"/\*.*?\*/", "", text, flags=re.DOTALL)
+
+    block_re = re.compile(r"(\w+)\s*\{(.*?)\}", re.DOTALL)
+    prop_re = re.compile(r"(\w+)\s*=\s*(.*?);")
+
+    for block_match in block_re.finditer(text):
+        block_type, content = block_match.groups()
+        props = {}
+        for prop_match in prop_re.finditer(content):
+            key, value = prop_match.groups()
+            props[key] = parse_value(value)
+
+        if block_type == "vertex":
+            x = props.pop("x", 0.0)
+            y = props.pop("y", 0.0)
+
+            vertices.append([x, y])
+
+        elif block_type == "linedef":
+            v1 = props.pop("v1", 0)
+            v2 = props.pop("v2", 0)
+
+            if "blocking" in props.keys():
+                blocking.append([v1, v2])
+            if "twosided" in props.keys():
+                twosided.append([v1, v2])
+            if "special" in props.keys():
+                special.append([v1, v2])
+
+        elif block_type == "thing":
+            x = props.pop("x", 0.0)
+            y = props.pop("y", 0.0)
+            type = props.pop("type", 0.0)
+
+            things.append([x, y, type])
+
+        else:
+            pass
+
+    blocking = np.array(blocking)
+    twosided = np.array(twosided)
+    special = np.array(special)
+    verts = np.array(vertices)
+
+    parsed_map.things = things
+
+    parsed_map.block = verts[blocking]
+    parsed_map.twosided = verts[twosided]
+    parsed_map.special = verts[special]
+
+    return parsed_map
+
+
+def parse_map(wad, map_name: str):
+    parsed_map = ParsedMap()
+    parsed_map.map_name = map_name
+    map_dict = wad.maps[map_name]
+    if "TEXTMAP" in map_dict.keys():
+        parsed_map = parse_udmf_format(wad, parsed_map)
+
+    elif "LINEDEF" in map_dict.keys():
+        parsed_map = parse_old_format(wad, parsed_map)
+
+    else:
+        logger.error(f"{map_name} map format is not recognised.")
+        return None
+
+    return parsed_map
